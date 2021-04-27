69bc69be81a5f6c1a1ac168acedacf8e933beb14
Your Nameadeyermi <63134506+adeyermi@users.noreply.github.com>
2021-04-27 23:13:24 +0100

    first commit

---
+++ blog/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from django.urls import reverse_lazy
+from .models import Post
+
+# Create your views here.
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = post_detail.html
+
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_name.html'
+    fields = ['title','author', 'body']
+
+class BlogUpdateView(UodateView)
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+class BlogDeleteView(Deleteview):
+    model = Post
+    template_name = 'post_delete.html'
+    sucess_url = reverse_lazy('home')
+
